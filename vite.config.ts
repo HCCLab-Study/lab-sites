fde15f0de0ddfca8c7fd1122178736792bcedd82
JongMany <blackberry1114@naver.com>
2024-02-18 01:00:56 +0900

    [ :sparkles: | 1 | Feat ] vite import alias 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	resolve: {
+		alias: [
+			{ find: '@', replacement: '/src' },
+			{ find: '@components', replacement: '/src/components' },
+			{ find: '@constants', replacement: '/src/constants' },
+			{ find: '@pages', replacement: '/src/pages' },
+			{ find: '@styles', replacement: '/src/styles' },
+			{ find: '@utils', replacement: '/src/utils' },
+			{ find: '@assets', replacement: '/src/assets' },
+			{ find: '@hooks', replacement: '/src/hooks' },
+			{ find: '@model', replacement: '/src/model' },
+			{ find: '@routes', replacement: '/src/routes' },
+			{ find: '@api', replacement: '/src/api' },
+		],
+	},
+});
