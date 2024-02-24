01b89507775f79f7252ac43a4f0dda0a44999047
iinuyha <122349402+iinuyha@users.noreply.github.com>
2024-02-24 15:19:55 +0900

    [ :sparkles: | #4 | Feat] section1 디자인 및 구현 완료

--- src/pages/people/_components/section1.tsx
+++ src/pages/people/_components/section1.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 // 타이틀부분
 function Section1() {
   return (
-    <section>
+    <section className='relative w-full	h-[397px] left-0 right-0 bg-gradient-to-br from-custom-red to-custom-blue'>
+      <div className="absolute bottom-0 inset-x-0 justify-center font-roboto font-medium text-5xl leading-6 flex items-center justify-center text-white" style={{ marginBottom: '50px' }}>
+        People
+      </div>
     </section>
   );
 }
