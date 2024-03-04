fcd85214b511f3b95bd20acdfe7bca1f95d381ad
minheewon <mhw1514@kw.ac.kr>
2024-03-05 02:44:15 +0900

    [ :sparkles: | #5 | Feat ] 색 설정 (아현님 코드 참고)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'custom-blue': '#2370C8',
+				'custom-red': '#A64D4D',
+				'custom-gray': '#646464',
+				'custom-navy': '#262A4E',
+				'font-gray': '#646464',
+				'section-gray' : '#F4F4F4',
+			  },
+		},
 	},
 	plugins: [],
 };
