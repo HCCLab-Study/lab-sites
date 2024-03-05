735b72c96400a155f0dedc74b284dca81bb79fd5
iinuyha <122349402+iinuyha@users.noreply.github.com>
2024-03-05 23:58:34 +0900

    [ :sparkles: | #16 | Feat ] 섹션별 파일 분류

---
+++ src/pages/projects/_components/section1.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// 타이틀부분
+function Section1() {
+  return (
+    <></>
+  );
+}
+
+export default Section1;
---
+++ src/pages/projects/_components/section3.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Finished Projects부분
+function Section3() {
+  return (
+    <></>
+  );
+}
+
+export default Section3;
