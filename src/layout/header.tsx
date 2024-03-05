9ac3fb89aa6692471debba47374162002baed828
홍유진 <asdfghjk1052@naver.com>
2024-03-05 19:10:42 +0900

    [ :wrench: | #6 | Fix] header 검색 버튼 이미지 변경

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -48,7 +48,9 @@ export default function Header() {
 						</li>
 						<div className="relative rounded-full inline-flex mt-1.1">
 							<input className="py-2 pl-4 pr-2 text-black rounded-full" type="text" placeholder="Search" />
-							<button className="absolute top-0 right-0 mt-2 mr-2">🔍</button>
+							<button className="absolute top-0 right-0 mt-2 mr-2">
+								<img src='src/assets/images/header/magnifying-glass.png' alt='magnifying-glass' className='w-6'/>
+							</button>
 						</div>
 					</ul>
 				</nav>
