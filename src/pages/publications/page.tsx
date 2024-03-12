8de8848851c8b7040a646af505c47e0d46fdb963
minheewon <mhw1514@kw.ac.kr>
2024-03-12 18:57:17 +0900

    [ :art: | #17 | Design ] page 높이 수정

--- src/pages/publications/page.tsx
+++ src/pages/publications/page.tsx
@@ -3,7 +3,7 @@ import Section2 from './_components/section2';
 
 export default function PublicationsPage() {
 	return (
-		<main className="overflow-x-hidden min-h-11">
+		<main className="min-h-11">
 			<Section1 />
 			<Section2 />
 		</main>
