7038ff318011e4f5235b3dc5e5003dae404cb76f
홍유진 <asdfghjk1052@naver.com>
2024-03-12 14:23:23 +0900

    [ :sparkles: | #20 | feature] lecture 페이지 추가

---
+++ src/pages/lectures/_components/section4.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+// 연구실 대학원 과정 정보
+const undergraudateCourses = [
+    { title: 'EXPERIMENTAL DESIGN AND ANALYSIS (실험설계및분석)',
+      semester: '2021-1, 2022-1, 2023-1, 2024-1',
+      description: '인간을 대상으로 실험을 계획하고, 이를 통해 데이터를 수집하고, 수집한 데이터를 분석하는 법을 다룬다. 최소의 실험횟수에서 최대의 정보를 얻기 위한 통계적 방법과 학문적 이론을 배우게 된다.'
+    },
+    { title: 'WEB SERVICE DESIGN AND PRACTICE \n(웹서비스설계및실습)',
+      semester: '2021-2, 2022-2, 2023-2',
+      description: '웹서비스를 개발하기 위해 필요한 이론/개념 및 프레임워크에 대해 학습한다. CRUD 및 Restful API의 개념을 익히고, javascript 기반의 프레임워크로 실습이 진행될 예정이다.'
+    },
+    { title: 'OPENSOURCE SOFTWARE\n(오픈소스 소프트웨어)',
+      semester: '2022-2, 2023-1, 2024-1',
+      description: '오픈소스 SW의 기본적인 개념 및 원리에 대해 이해하고, 오픈소스 SW 개발 과정의 각 단계에서 주로 사용되는 도구들의 실무적인 활용 능력을 키운다. 또한 애자일 개발방법론, 디자인패턴, 테스트 방법론 등 소프트웨어공학의 기초적인 지식을 함양한다.'
+    },
+    { title: 'INFORMATION DESIGN AND PROGRAMMING (정보디자인 프로그래밍)',
+      semester: '2020-2, 2021-1, 2022-1, 2023-2',
+      description: '정보디자인에 대한 이론을 기반으로 웹 환경에서 정보를 효과적으로 설계하고 시각화하는 방법을 학습한다. (Javascript, d3.js 실습 및 Front-end 소개)'
+    },
+    { title: 'INTRODUCTION OF CREATIVE DESIGN\n(창의설계입문)',
+      semester: '2020-2, 2021-1',
+      description: '복잡한 문제를 창의적으로 해결하기 위해 각광받고 있는 디자인 사고 (Design Thinking)의 개념과 프로세스에 대해 학습한다. 실제 문제에 적용하여 해결하는 과정을 통해 디자인사고 능력을 내재화한다.'
+    },
+    { title: 'APPLICATIONS OF STATISTICS (통계학응용)',
+      semester: '2021-2',
+      description: 'SW를 전공하고 있는 학생을 위한 기본적인 확률 및 통계에 대한 이론을 다룬다. 확률변수, 분포, 추정, 검정에 대한 개념을 배우게 된다.'
+    },
+    { title: 'VISUAL COMPUTING (비주얼컴퓨팅)',
+    semester: '2020-2',
+    description: '비주얼테크놀로지의 기반이 되는 영상처리, 컴퓨터비전, 컴퓨터그래픽스의 전반적인 기초 이론과 실습을 다룬다. (openCV & openGL 실습)'
+    },
+    ];
+
+function Section4() {
+    return (
+        <section className='relative w-full h-[1300px] left-0 right-0'>
+            <div className='absolute h-[1200px] w-[1000px] left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 font-roboto text-custom-navy'>
+                <div className='absolute h-[50px] w-full border-b-2 border-custom-blue'>
+                    <h2 className='absolute flex items-center text-3xl font-semibold tracking-widest'>UNDERGRADUATE COURSEWORK (학부)</h2>
+                </div>
+                <div className='pt-16 pl-12'>
+                    <ul className='grid grid-cols-2 gap-16 list-disc'>
+                        {undergraudateCourses.map((undergraudateCourses, index) => (
+                            <li key={index} className='font-semibold p-4 pb-2 text-xl whitespace-pre-wrap'>
+                                {undergraudateCourses.title}
+                                <p className='text-font-gray text-base pt-6'>개설년도/학기: {undergraudateCourses.semester}</p>
+                                <p className='text-font-gray text-base font-normal pt-2'>{undergraudateCourses.description}</p>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Section4;
--- src/pages/lectures/page.tsx
+++ src/pages/lectures/page.tsx
@@ -1,3 +1,15 @@
+import Section1 from './_components/section1';
+import Section2 from './_components/section2';
+import Section3 from './_components/section3';
+import Section4 from './_components/section4';
+
 export default function LecturesPage() {
-	return <main>강의페이지</main>;
+	return ( 
+		<main className='mt-14'>
+			<Section1 />
+			<Section2 />
+			<Section3 />
+			<Section4 />
+		</main>
+	);
 }
