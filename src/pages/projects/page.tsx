8cd6a788977bc09bfc0db4896929233e3f8e5419
iinuyha <122349402+iinuyha@users.noreply.github.com>
2024-03-13 13:09:27 +0900

    [ :sparkles: | #26 | Feat] projects.mockup.ts파일 완료

--- src/pages/projects/page.tsx
+++ src/pages/projects/page.tsx
@@ -3,6 +3,12 @@ import Section2 from './_components/section2';
 import Section3 from './_components/section3';
 
 export default function ProjectsPage() {
+
+	// 여기서 쿼리클라이언트 사용하여 Section1, 2, 3에서 사용할 쿼리키를 설정할 수 있음
+	// 쿼리키와 그에 맞는 메서드(어떤 데이터를 사용할 지)를 여기서 설정주면 됨
+	// useQuery 이용하여 쿼리키별로 어떤 내용을 담을지 어떤 api를 받아올지 키를 설정해주는거..
+	// 그럼 이제 Section1, 2, 3에서는 키만 입력해서 원하는 데이터를 받아올 수 있는거임
+
 	return (
 		<main className='mt-14'>
 			<Section1 />
