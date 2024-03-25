048f50dcecf54762382113e3d615ce1fb26caa6c
minheewon <mhw1514@kw.ac.kr>
2024-03-25 17:50:22 +0900

    [ :sparkles: | #31 | Feat ] Publications 페이지 MSW 수정

--- src/pages/publications/page.tsx
+++ src/pages/publications/page.tsx
@@ -1,11 +1,18 @@
 import Section1 from './_components/section1';
 import Section2 from './_components/section2';
+import { Journals, Conferences } from '../../model/publication.type';
+import { useGetAllPublications } from './_hooks/useGetAllPublications';
 
 export default function PublicationsPage() {
+	const { data: publication, isFetching, isError } = useGetAllPublications();
+
+	const journals: Journals[] = publication?.journals || [];
+	const conferences: Conferences[] = publication?.conferences || [];
+
 	return (
-		<main className='mt-14'>
+		<main className="mt-14">
 			<Section1 />
-			<Section2 />
+			<Section2 journals={journals} conferences={conferences} />
 		</main>
 	);
 }
