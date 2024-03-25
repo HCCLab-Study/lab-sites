04974aaf703aae7185b97b8256ce61f9f4f62747
iinuyha <122349402+iinuyha@users.noreply.github.com>
2024-03-25 16:52:36 +0900

    [ :fix: | #34 | Fix ] home css 수정

--- src/pages/home/_components/section2.tsx
+++ src/pages/home/_components/section2.tsx
@@ -2,11 +2,11 @@
 function Section2() {
     return (
         <section className='relative w-full	h-[400px] left-0 right-0'>
-            <div className='absolute h-[350px] w-[1700px] left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 font-roboto text-center'>
+            <div className='absolute h-[350px] w-[1000px] left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 font-roboto text-center'>
                 <div className="text-custom-blue font-semibold text-xl tracking-widest" style={{marginTop: '100px', marginBottom: '30px'}}>
                     Introduction
                 </div>
-                <div className="text-custom-navy font-semibold text-5xl tracking-tight" style={{marginBottom: '30px'}}>
+                <div className="text-custom-navy font-semibold text-4xl tracking-tight" style={{marginBottom: '30px'}}>
                     Where Technology Meets Humanity
                 </div>
                 <div className='flex justify-center items-center'>
