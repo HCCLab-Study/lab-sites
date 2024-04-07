4f53bb98634cabd1bf0a7e836f5da29ee1342b89
minheewon <mhw1514@kw.ac.kr>
2024-04-07 22:54:54 +0900

    [ :wrench: | #42 | Fix ] People Page MSW 삭제 및 되돌리기

--- src/pages/people/_components/section7.tsx
+++ src/pages/people/_components/section7.tsx
@@ -2,48 +2,47 @@ import React from 'react';
 
 // Alumni 부분
 function Section7() {
-  return (
-    <section className='relative w-full	h-[400px] left-0 right-0 bg-section-gray'>
-      <div className='absolute h-[350px] w-[1000px] left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 font-roboto text-custom-navy'>
-        <div className='absolute h-[50px] w-full border-b-2 border-custom-blue'>
-          <h2 className='absolute flex items-center text-3xl font-semibold tracking-widest'>Alumni</h2>
-        </div>
-        <div className="flex flex-row relative top-[50px]">
-          <div className='flex-1'>
-            <h3 className='text-custom-blue font-semibold text-base py-4'>대학원생 (Graduate Researcher)</h3>
-            <ul className='list-disc list-inside text-sm pb-4'>
-              <li className='mb-2'>김현수 (MS)	2021.09 ~ 2024.02</li>
-            </ul>
-          </div>
-          <div className='flex-1'>
-            <h3 className='text-custom-blue font-semibold text-base py-4'>학부연구생 (Undergraduate Researcher)</h3>
-            <ul className='list-disc list-inside text-sm pb-4'>
-              <li className='mb-2'>김종민 2021.07 ~ 2022.12</li>
-              <li className='mb-2'>신수빈 2021.06 ~ 2022.12</li>
-              <li className='mb-2'>정범영 2021.01 ~ 2021.12</li>
-              <li className='mb-2'>이영현 2021.05 ~ 2023.08</li>
-              <li className='mb-2'>박제인 2023.06 ~ 2023.08</li>
-              <li className='mb-2'>황희태 2022.06 ~ 2024.01</li>
-            </ul>
-          </div>
-          <div className='flex-1'>
-            <h3 className='text-custom-blue font-semibold text-base py-4'>대학원생 (Graduate Researcher)</h3>
-            <ul className='list-disc list-inside text-sm pb-4'>
-              <li className='mb-2'>김채현 2021.07 ~ 2022.12</li>
-              <li className='mb-2'>이현빈 2022.04 ~ 2022.07</li>
-              <li className='mb-2'>채수지 2021.01 ~ 2022.06</li>
-              <li className='mb-2'>이규민 2022.01 ~ 2023.02</li>
-              <li className='mb-2'>신해담 2022.07 ~ 2023.02</li>
-              <li className='mb-2'>홍성혁 2022.04 ~ 2023.06</li>
-              <li className='mb-2'>김형빈 2022.07 ~ 2023.06</li>
-              <li className='mb-2'>변유진 2022.04 ~ 2024.02</li>
-            </ul>
-          </div>
-        </div>
-
-      </div>
-    </section>
-  );
+	return (
+		<section className="relative w-full	h-[400px] left-0 right-0 bg-section-gray">
+			<div className="absolute h-[350px] w-[1000px] left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 font-roboto text-custom-navy">
+				<div className="absolute h-[50px] w-full border-b-2 border-custom-blue">
+					<h2 className="absolute flex items-center text-3xl font-semibold tracking-widest">Alumni</h2>
+				</div>
+				<div className="flex flex-row relative top-[50px]">
+					<div className="flex-1">
+						<h3 className="text-custom-blue font-semibold text-base py-4">대학원생 (Graduate Researcher)</h3>
+						<ul className="list-disc list-inside text-sm pb-4">
+							<li className="mb-2">김현수 (MS) 2021.09 ~ 2024.02</li>
+						</ul>
+					</div>
+					<div className="flex-1">
+						<h3 className="text-custom-blue font-semibold text-base py-4">학부연구생 (Undergraduate Researcher)</h3>
+						<ul className="list-disc list-inside text-sm pb-4">
+							<li className="mb-2">김종민 2021.07 ~ 2022.12</li>
+							<li className="mb-2">신수빈 2021.06 ~ 2022.12</li>
+							<li className="mb-2">정범영 2021.01 ~ 2021.12</li>
+							<li className="mb-2">이영현 2021.05 ~ 2023.08</li>
+							<li className="mb-2">박제인 2023.06 ~ 2023.08</li>
+							<li className="mb-2">황희태 2022.06 ~ 2024.01</li>
+						</ul>
+					</div>
+					<div className="flex-1">
+						<h3 className="text-custom-blue font-semibold text-base py-4">대학원생 (Graduate Researcher)</h3>
+						<ul className="list-disc list-inside text-sm pb-4">
+							<li className="mb-2">김채현 2021.07 ~ 2022.12</li>
+							<li className="mb-2">이현빈 2022.04 ~ 2022.07</li>
+							<li className="mb-2">채수지 2021.01 ~ 2022.06</li>
+							<li className="mb-2">이규민 2022.01 ~ 2023.02</li>
+							<li className="mb-2">신해담 2022.07 ~ 2023.02</li>
+							<li className="mb-2">홍성혁 2022.04 ~ 2023.06</li>
+							<li className="mb-2">김형빈 2022.07 ~ 2023.06</li>
+							<li className="mb-2">변유진 2022.04 ~ 2024.02</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default Section7;
