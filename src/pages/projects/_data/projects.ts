2620f7f0aca9157efa2071fcd11cec2c2d7acf3e
iinuyha <122349402+iinuyha@users.noreply.github.com>
2024-04-08 17:36:58 +0900

    [ :branch: | #47 | Fix ] project페이지 팝업 만들기 완료

--- src/pages/projects/_data/projects.ts
+++ src/pages/projects/_data/projects.ts
@@ -5,25 +5,25 @@ const onGoingProjects: Project[] = [
         date: "2023.10 - 2025.09",
         title: "머신러닝 기반 파킨슨병 환자의 변화 지표 모니터링과 맞춤형 자조관리 시스템",
         organization: "중소벤처기업부",
-        content: "d"
+        content: "projects/_data/projects.ts 파일에 프로젝트 객체를 만들어뒀는데요, content에 적는 내용이 여기에 보이게 됩니다.. 프로젝트 관련 설명같은거 적기................................ 내용이 팝업창보다 많아지면 스크롤바 생김!!!!!!"
     },
     {
         date: "2022.08 - 2024.07",
         title: "파킨슨병 환자의 신체기능 모니터링 및 운동관리를 위한 스마트폰 어플리케이션 개발 및 임상시험",
         organization: "서울대학교병원",
-        content: "d"
+        content: "projects/_data/projects.ts 파일에 프로젝트 객체를 만들어뒀는데요, content에 적는 내용이 여기에 보이게 됩니다.. 프로젝트 관련 설명같은거 적기................................ 내용이 팝업창보다 많아지면 스크롤바 생김!!!!!!"
     },
     {
         date: "2022.07 - 2025.02",
         title: "실시간 온라인교육에서 학습몰입과 참여를 강화하기 위한 AI 알고리즘 기반 맞춤형 피드백 시스템 개발",
         organization: "National Research Foundation of Korea, 한국연구재단",
-        content: "d"
+        content: "projects/_data/projects.ts 파일에 프로젝트 객체를 만들어뒀는데요, content에 적는 내용이 여기에 보이게 됩니다.. 프로젝트 관련 설명같은거 적기................................ 내용이 팝업창보다 많아지면 스크롤바 생김!!!!!!"
     },
     {
         date: "2021.03 - 2024.02",
         title: "신뢰 조절을 활용한 자율주행시스템의 운전자 경험 설계 및 모델링 Experience Design and Modeling of Autonomous Driving System using Trust Calibration",
         organization: "National Research Foundation of Korea, 한국연구재단",
-        content: "d"
+        content: "projects/_data/projects.ts 파일에 프로젝트 객체를 만들어뒀는데요, content에 적는 내용이 여기에 보이게 됩니다.. 프로젝트 관련 설명같은거 적기................................ 내용이 팝업창보다 많아지면 스크롤바 생김!!!!!!"
     }
 ];
 
@@ -32,133 +32,133 @@ const finishedProjects = [
         date: "2021.03 - 2024.02",
         title: "신뢰 조절을 활용한 자율주행시스템의 운전자 경험 설계 및 모델링 Experience Design and Modeling of Autonomous Driving System using Trust Calibration",
         organization: "National Research Foundation of Korea, 한국연구재단",
-        content: "d"
+        content: "projects/_data/projects.ts 파일에 프로젝트 객체를 만들어뒀는데요, content에 적는 내용이 여기에 보이게 됩니다.. 프로젝트 관련 설명같은거 적기................................ 내용이 팝업창보다 많아지면 스크롤바 생김!!!!!!"
     },
     {
         date: "2021.04 - 2023.12",
         title: "청각보조기기 현장 실증 및 사용성 평가 Development of hearing aids that conveniently receive sound and subtitles",
         organization: "보건복지부",
-        content: "d"
+        content: "projects/_data/projects.ts 파일에 프로젝트 객체를 만들어뒀는데요, content에 적는 내용이 여기에 보이게 됩니다.. 프로젝트 관련 설명같은거 적기................................ 내용이 팝업창보다 많아지면 스크롤바 생김!!!!!!"
     },
     {
         date: "2021.09 - 2023.11",
         title: "의료적 관리가 필요한 장애인을 위한 재활운동기기 개발 및 임상 중개 연구-상지운동기기",
         organization: "국립재활원",
-        content: "s"
+        content: "projects/_data/projects.ts 파일에 프로젝트 객체를 만들어뒀는데요, content에 적는 내용이 여기에 보이게 됩니다.. 프로젝트 관련 설명같은거 적기................................ 내용이 팝업창보다 많아지면 스크롤바 생김!!!!!!"
     },
     {
         date: "2023.06 - 2023.08",
         title: "당뇨 환자 눈 관리 시스템의 사용자 인터페이스 및 사용자 경험 설계",
         organization: "주식회사 클롭",
-        content: "d"
+        content: "projects/_data/projects.ts 파일에 프로젝트 객체를 만들어뒀는데요, content에 적는 내용이 여기에 보이게 됩니다.. 프로젝트 관련 설명같은거 적기................................ 내용이 팝업창보다 많아지면 스크롤바 생김!!!!!!"
     },
     {
         date: "2022.04 - 2023.03",
         title: "인공지능 기반 파킨슨병 환자의 자가 훈련과 모니터링 솔루션 개발 및 사업화 Development and Commercialization of AI-based Self-training and Monitoring Solutions for Patients with Parkinsonism",
         organization: "IITP, 정보통신기획평가원",
-        content: "d"
+        content: "projects/_data/projects.ts 파일에 프로젝트 객체를 만들어뒀는데요, content에 적는 내용이 여기에 보이게 됩니다.. 프로젝트 관련 설명같은거 적기................................ 내용이 팝업창보다 많아지면 스크롤바 생김!!!!!!"
     },
     {
         date: "2022.02 - 2022.07",
         title: "웹 쇼핑몰의 고객 경험 분석 및 그로스해킹 컨설팅",
         organization: "Designovel, 디자이노블",
-        content: "d"
+        content: "projects/_data/projects.ts 파일에 프로젝트 객체를 만들어뒀는데요, content에 적는 내용이 여기에 보이게 됩니다.. 프로젝트 관련 설명같은거 적기................................ 내용이 팝업창보다 많아지면 스크롤바 생김!!!!!!"
     },
     {
         date: "2022.02 - 2022.06",
         title: "브랜드 악용 탐지 솔루션의 대시보드 설계 및 개발",
         organization: "S2W, 에스투더블유",
-        content: "d"
+        content: "projects/_data/projects.ts 파일에 프로젝트 객체를 만들어뒀는데요, content에 적는 내용이 여기에 보이게 됩니다.. 프로젝트 관련 설명같은거 적기................................ 내용이 팝업창보다 많아지면 스크롤바 생김!!!!!!"
     },
     {
         date: "2021.09 - 2022.02",
         title: "지능형 로봇을 위한 기초수학 분야의 학습 패턴 분석 및 교육 개선안 도출",
         organization: "한양대학교 산학협력단",
-        content: "d"
+        content: "projects/_data/projects.ts 파일에 프로젝트 객체를 만들어뒀는데요, content에 적는 내용이 여기에 보이게 됩니다.. 프로젝트 관련 설명같은거 적기................................ 내용이 팝업창보다 많아지면 스크롤바 생김!!!!!!"
     },
     {
         date: "2021.01 - 2021.12",
         title: "발달장애인의 의사소통 효율성 증대를 위한 AAC 검색 기능 및 표현 예측 알고리즘 개발",
         organization: "SOVORO, 소리를보는통로",
-        content: "d"
+        content: "projects/_data/projects.ts 파일에 프로젝트 객체를 만들어뒀는데요, content에 적는 내용이 여기에 보이게 됩니다.. 프로젝트 관련 설명같은거 적기................................ 내용이 팝업창보다 많아지면 스크롤바 생김!!!!!!"
     },
     {
         date: "2020.07 - 2020.09",
         title: "파킨슨병환자를 위한 운동 처방 및 관리 어플리케이션 개발 Development of Exercise Prescription and Management Application for Parkinson's disease",
         organization: "Seoul National University Hospital, 서울대학교병원",
-        content: "d"
+        content: "projects/_data/projects.ts 파일에 프로젝트 객체를 만들어뒀는데요, content에 적는 내용이 여기에 보이게 됩니다.. 프로젝트 관련 설명같은거 적기................................ 내용이 팝업창보다 많아지면 스크롤바 생김!!!!!!"
     },
     {
         date: "2020.02 - 2020.08",
         title: "데이터 기반 UX/CX 분석 방법론 개발 Data-driven UX/CX Analysis methodology",
         organization: "Samsung SDS",
-        content: "d"
+        content: "projects/_data/projects.ts 파일에 프로젝트 객체를 만들어뒀는데요, content에 적는 내용이 여기에 보이게 됩니다.. 프로젝트 관련 설명같은거 적기................................ 내용이 팝업창보다 많아지면 스크롤바 생김!!!!!!"
     },
     {
         date: "2019.07 - 2019.11",
         title: "컨택센터의 CX 연구 방법론 설계 CX Research methodology in Contact Center",
         organization: "Samsung Electronics America & Samsung SDS America",
-        content: "d"
+        content: "projects/_data/projects.ts 파일에 프로젝트 객체를 만들어뒀는데요, content에 적는 내용이 여기에 보이게 됩니다.. 프로젝트 관련 설명같은거 적기................................ 내용이 팝업창보다 많아지면 스크롤바 생김!!!!!!"
     },
     {
         date: "2018.08 - 2018.12",
         title: " Blockchain Platform with Business Models towards Cross-domain Interoperability",
         organization: "Ministry of Science and ICT, 미래창조과학부",
-        content: "d"
+        content: "projects/_data/projects.ts 파일에 프로젝트 객체를 만들어뒀는데요, content에 적는 내용이 여기에 보이게 됩니다.. 프로젝트 관련 설명같은거 적기................................ 내용이 팝업창보다 많아지면 스크롤바 생김!!!!!!"
     },
     {
         date: "2018.01 - 2018.09",
         title: "모바일 뱅킹앱에 대한 사용성 평가 Usability Evaluation for Financial Mobile Applications and its Digital Strategy",
         organization: "BNK Financial Group, BNK금융그룹",
-        content: "d"
+        content: "projects/_data/projects.ts 파일에 프로젝트 객체를 만들어뒀는데요, content에 적는 내용이 여기에 보이게 됩니다.. 프로젝트 관련 설명같은거 적기................................ 내용이 팝업창보다 많아지면 스크롤바 생김!!!!!!"
     },
     {
         date: "2017.07 - 2017.11",
         title: "프로세스마이닝 기반 시스템 로그와 UX 분석 모델 개발 Development of System Log and UX analysis Model based on Process Mining",
         organization: "Samsung Heavy Industries, 삼성중공업",
-        content: "d"
+        content: "projects/_data/projects.ts 파일에 프로젝트 객체를 만들어뒀는데요, content에 적는 내용이 여기에 보이게 됩니다.. 프로젝트 관련 설명같은거 적기................................ 내용이 팝업창보다 많아지면 스크롤바 생김!!!!!!"
     },
     {
         date: "2016.06 - 2017.07",
         title: "Development of supportive tools for enhancing pedestrian experience of the mobility handicapped based on IoT",
         organization: "National Research Foundation of Korea, 한국연구재단",
-        content: "d"
+        content: "projects/_data/projects.ts 파일에 프로젝트 객체를 만들어뒀는데요, content에 적는 내용이 여기에 보이게 됩니다.. 프로젝트 관련 설명같은거 적기................................ 내용이 팝업창보다 많아지면 스크롤바 생김!!!!!!"
     },
     {
         date: "2016.05 - 2017.07",
         title: "Development of a statistical analysis program for LKAS (Lane Keeping Assist System) performance evaluation",
         organization: "Hyundai Motor Company, 현대자동차",
-        content: "d"
+        content: "projects/_data/projects.ts 파일에 프로젝트 객체를 만들어뒀는데요, content에 적는 내용이 여기에 보이게 됩니다.. 프로젝트 관련 설명같은거 적기................................ 내용이 팝업창보다 많아지면 스크롤바 생김!!!!!!"
     },
     {
         date: "2016.06 - 2016.10",
         title: "A Study on the Multi-Device Usage of Millennials",
         organization: "Samsung Electronics, 삼성전자",
-        content: "d"
+        content: "projects/_data/projects.ts 파일에 프로젝트 객체를 만들어뒀는데요, content에 적는 내용이 여기에 보이게 됩니다.. 프로젝트 관련 설명같은거 적기................................ 내용이 팝업창보다 많아지면 스크롤바 생김!!!!!!"
     },
     {
         date: "2016.03 - 2016.08",
         title: "User experience evaluation system for consumer electronic products to enhance competitiveness of small and medium enterprises",
         organization: "Ministry of Trade, Industry and Energy, 산업통상자원부",
-        content: "d"
+        content: "projects/_data/projects.ts 파일에 프로젝트 객체를 만들어뒀는데요, content에 적는 내용이 여기에 보이게 됩니다.. 프로젝트 관련 설명같은거 적기................................ 내용이 팝업창보다 많아지면 스크롤바 생김!!!!!!"
     },
     {
         date: "2015.07 - 2016.02",
         title: "Research on the Development of Embodied Participatory Simulation and its Effects from Embodied Cognition Perspectives",
         organization: "National Research Foundation of Korea, 한국연구재단",
-        content: "d"
+        content: "projects/_data/projects.ts 파일에 프로젝트 객체를 만들어뒀는데요, content에 적는 내용이 여기에 보이게 됩니다.. 프로젝트 관련 설명같은거 적기................................ 내용이 팝업창보다 많아지면 스크롤바 생김!!!!!!"
     },
     {
         date: "2014.07 - 2016.02",
         title: "POA: Designing a Gamification-Based Social Learning Application",
         organization: "미래IT융합연구소",
-        content: "d"
+        content: "projects/_data/projects.ts 파일에 프로젝트 객체를 만들어뒀는데요, content에 적는 내용이 여기에 보이게 됩니다.. 프로젝트 관련 설명같은거 적기................................ 내용이 팝업창보다 많아지면 스크롤바 생김!!!!!!"
     },
     {
         date: "2014.03 - 2014.10",
         title: "Web-page Clustering Method focusing on Similarity Measures",
         organization: "National Security Research Institute, 국가보안기술연구소",
-        content: "d"
+        content: "projects/_data/projects.ts 파일에 프로젝트 객체를 만들어뒀는데요, content에 적는 내용이 여기에 보이게 됩니다.. 프로젝트 관련 설명같은거 적기................................ 내용이 팝업창보다 많아지면 스크롤바 생김!!!!!!"
     }
 ];
 
