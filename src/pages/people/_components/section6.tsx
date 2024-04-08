bf2fb63a7c9b733e000318c3cd9514b65d527efb
minheewon <mhw1514@kw.ac.kr>
2024-04-09 02:56:16 +0900

    [ :wrench: | #49 | Fix ] People Page 오타 수정

--- src/pages/people/_components/section6.tsx
+++ src/pages/people/_components/section6.tsx
@@ -64,9 +64,7 @@ function Section6() {
 		<section className="relative w-full	h-[700px] left-0 right-0">
 			<div className="absolute h-[650px] w-[1000px] left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 font-roboto text-custom-navy">
 				<div className="absolute h-[50px] w-full border-b-2 border-custom-blue">
-					<h2 className="absolute flex items-center text-3xl font-semibold tracking-widest">
-						Undergraduate Researchers
-					</h2>
+					<h2 className="absolute flex items-center text-3xl font-semibold tracking-widest">Developer Intership</h2>
 				</div>
 				<div className="flex flex-row relative top-[50px]">
 					{developerInternshipData.slice(0, 5).map((person, index) => (
